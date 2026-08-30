ac69d404813f0af73509a286509190391510e89e
Ruthwik <chittetiruthwik@gmail.com>
2026-08-30 17:54:59 +0530

    Delete src directory

--- src/lib/math.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-
-/** JS-side smoothstep (GLSL has its own in shaders). */
-export const smoothstep = (a, b, x) => {
-  const t = THREE.MathUtils.clamp((x - a) / (b - a), 0, 1)
-  return t * t * (3 - 2 * t)
-}
-
-export const fract = (v) => v - Math.floor(v)
